01aa52fc5f7b8d9cb62e0ed314d96c6277cb71b2
Doug DiFilippo <ddifilippo@wespeke.com>
2016-01-17 09:48:32 -0500

    Adds a basic ws.send to sender html

--- socket-server.js
+++ socket-server.js
@@ -11,18 +11,24 @@ server.listen(port)
 
 console.log("http server listening on %d", port)
 
-var wss = new WebSocketServer({server: server})
+var wss = new WebSocketServer({
+    server: server
+})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(new Date()), function() {})
+    }, 1000);
 
-  console.log("websocket connection open")
+    ws.on('message', function(msg){
+        console.log('=====================');
+        console.log(msg);
+        console.log('=====================');
+    });
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
+    ws.on("close", function() {
+        console.log("websocket connection close")
+        clearInterval(id)
+    })
 })
